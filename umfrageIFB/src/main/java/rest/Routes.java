762e7a3e53c0918f2a5e75e3f67b1e28f70dae31
Unknown <michael.nickel@fh-bielefeld.de>
2019-01-23 00:56:56 +0100

    versions ordner

--- umfrageIFB/src/main/java/rest/Routes.java
+++ umfrageIFB/src/main/java/rest/Routes.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Routes {
 	
-	@RequestMapping("/")
+	 @RequestMapping("/")
 	public String index() {
 		return "forward:/index.html";
-	}
+	} 
 }
