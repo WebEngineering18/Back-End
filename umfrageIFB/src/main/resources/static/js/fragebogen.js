27c725be440001b8986a709bb4aa9482f41e6a2d
Unknown <michael.nickel@fh-bielefeld.de>
2019-01-24 18:31:31 +0100

    Verbesserung einiger Fragen

--- umfrageIFB/src/main/resources/static/js/fragebogen.js
+++ umfrageIFB/src/main/resources/static/js/fragebogen.js
@@ -15,18 +15,18 @@ window.onload = function () {
                 { question_id: 4, answer: ["0", "0"], type: "100%" }, //2 numbers
                 { question_id: 5, answer: [], type: "year-choose" }, //every year or all years
                 { question_id: 6, answer: ["0", "0", "0", "0"], no_answer: [], type: "100%" }, //4 numbers choose
-                { question_id: 7, answer: [], type: "number" }, //number
+                { question_id: 7, answer: [], type: "singlePercentage" }, //number
                 { question_id: 8, answer: [], other: "", type: "checkbox" }, //checkbox with other text
                 { question_id: 9, answer: [], type: "year-choose" }, //every year or all years
                 { question_id: 10, answer: ["0", "0", "0", "0", "0", "0", "0", "0", "0"], type: "100%" }, //2 numbers
                 { question_id: 11, answer: [], type: "checkbox" }, //checkbox
-                { question_id: 12, answer: [], type: "number" }, //checkbox
+                { question_id: 12, answer: [], type: "singlePercentage" }, //checkbox
                 { question_id: 13, answer: [], other: "", type: "checkbox" }, //checkbox with other text
                 { question_id: 14, answer: ["0", "0", "0"], type: "100%" }, //3 numbers
-                { question_id: 15, answer: [], type: "number" }, //all years and every year
-                { question_id: 16, answer: [], type: "number" }, //all years and every year
+                { question_id: 15, answer: [], type: "year-choose" }, //all years and every year
+                { question_id: 16, answer: [], type: "year-choose" }, //all years and every year
                 { question_id: 17, answer: [], type: "number" }, //number
-                { question_id: 18, answer: [], type: "number" }, //all years and every year
+                { question_id: 18, answer: [], type: "year-choose" }, //all years and every year
                 { question_id: 19, answer: [], type: "radio" }, //radio
                 { question_id: 20, answer: [], type: "radio" }, //radio
             ],
@@ -159,8 +159,8 @@ window.onload = function () {
                     } else {
                         this.error = false;
                     }
-                } else if (this.answers[this.questionIndex - 1].type == "year-choose" || this.answers[this.questionIndex - 1].type == "everyYear" || this.answers[this.questionIndex - 1].type == "2013-2017") {
-                    if (this.answers[this.questionIndex - 1].answer.length <= 0) {
+                } else if (this.answers[this.questionIndex - 1].type == "2013-2017" || this.answers[this.questionIndex - 1].type == "year-choose") {
+                    if (this.answers[this.questionIndex - 1].answer <= 0) {
                         this.errorMessage = "Sie müssen ein Feld markieren und ausfüllen!";
                         this.error = true;
                     } else {
@@ -173,9 +173,18 @@ window.onload = function () {
                     } else {
                         this.error = false;
                     }
+                } else if (this.answers[this.questionIndex - 1].type == "everyYear") {
+
+                    if (this.answers[this.questionIndex - 1].answer.length != 5) {
+                        this.errorMessage = "Sie müssen für jedes Jahr eine Zahl eintragen!"
+                        this.error = true;
+                    } else {
+                        this.error = false;
+                    }
+
                 }
-                if (this.answers[this.questionIndex - 1].question_id == 12 || this.answers[this.questionIndex - 1].question_id == 7) {
-                    let number = parseInt(this.answers[this.questionIndex -1].answer);
+                else if (this.answers[this.questionIndex - 1].type == "singlePercentage") {
+                    let number = parseInt(this.answers[this.questionIndex - 1].answer);
                     if (number > 100) {
                         this.errorMessage = "Der Wert darf nicht größer als 100 sein.";
                         this.error = true;
